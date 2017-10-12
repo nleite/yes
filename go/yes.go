c45f7949bfa120d111f488c9860300a39327b313
nleite <norberto@10gen.com>
2017-10-12 18:58:43 -0400

    go: using buffer - dramatic performance boost 3.0MBi/s -> 2Gbi/s

--- go/yes.go
+++ go/yes.go
@@ -1,10 +1,16 @@
 package main
 
 import "os"
+import "bytes"
 
 func main() {
-	y := []byte("y\n")
+	var buffer bytes.Buffer
+	const BFSIZE = 1204
+	// allocate buffer of size BFSIZE
+	for i := 0; i < BFSIZE; i++ {
+		buffer.WriteString("y\n")
+	}
 	for true {
-		os.Stdout.Write(y)
+		os.Stdout.Write(buffer.Bytes())
 	}
 }

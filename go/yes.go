a4cf7b4fd065c4a364c20620156dd71742ddfe6e
nleite <norberto@10gen.com>
2017-10-12 21:02:50 -0400

    go: buffer size to 8192 - even better performance 2.0GBi/s -> 6.0GBi/s

--- go/yes.go
+++ go/yes.go
@@ -5,7 +5,7 @@ import "bytes"
 
 func main() {
 	var buffer bytes.Buffer
-	const BFSIZE = 1204
+	const BFSIZE = 8192
 	// allocate buffer of size BFSIZE
 	for i := 0; i < BFSIZE; i++ {
 		buffer.WriteString("y\n")
